64215683440ca5b650617d08f8ccd50d0219fd85
Lutz <lutz.sauer@yahoo.de>
2023-10-03 20:56:26 +0200

    add settings CSRF_TRUSTED_ORIGINS

--- mytraveldiarydummy/settings.py
+++ mytraveldiarydummy/settings.py
@@ -129,6 +129,10 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://my-travel-diary.de'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
