84868bcdb56178bd78f03fc33f78b70b0ae34218
Lutz <lutz.sauer@yahoo.de>
2023-10-03 18:47:38 +0200

    add database settings

--- mytraveldiarydummy/settings.py
+++ mytraveldiarydummy/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = "mytraveldiarydummy.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'railway',
+        "URL":'postgresql://postgres:jgKmF9kqmg8hjAKHKIJh@containers-us-west-101.railway.app:5664/railway',
+        'USER': 'postgres',
+        'PASSWORD': 'jgKmF9kqmg8hjAKHKIJh',
+        'HOST': 'containers-us-west-101.railway.app',
+        'PORT': '5664',        
     }
 }
 
