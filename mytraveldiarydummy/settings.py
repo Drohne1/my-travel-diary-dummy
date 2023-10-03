b4cf4f5af89a5992c8eb37c636d4c853cafa57fb
Lutz <lutz.sauer@yahoo.de>
2023-10-03 21:24:45 +0200

    add mimetypes

--- mytraveldiarydummy/settings.py
+++ mytraveldiarydummy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-we6$))re2qz$phk_md9-y=b!ew^@-z=5z8oaomhs)(d_)@#u$a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = [
@@ -132,6 +134,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CSRF_TRUSTED_ORIGINS = [
     'https://my-travel-diary.de'
 ]
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
